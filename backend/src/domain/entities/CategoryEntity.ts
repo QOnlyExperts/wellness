4e7d70ff9f880df9306737d112dc6fc3fa95656a
Stephen Chica <stephenalexandergomez@gmail.com>
2025-10-19 11:37:19 -0500

    mapper y repository de category

--- backend/src/domain/entities/CategoryEntity.ts
+++ backend/src/domain/entities/CategoryEntity.ts
@@ -1,21 +1,28 @@
-// src/domain/entities/Category.ts
+// src/domain/entities/CategoryEntity.ts
 
-export class Category {
+export class CategoryEntity {
   public readonly id: number;
   public name: string;
   public description: string | null;
 
-  constructor({
-    id,
-    name,
-    description,
-  }: {
+  // Hacemos el constructor privado para forzar la creación a través del método 'create'
+  private constructor(props: {
     id: number;
     name: string;
-    description?: string;
+    description?: string | null;
   }) {
-    this.id = id;
-    this.name = name;
-    this.description = description ?? null;
+    this.id = props.id;
+    this.name = props.name;
+    this.description = props.description ?? null;
+  }
+
+  // Este es el método estático que usa el Mapper
+  public static create(props: {
+    id: number;
+    name: string;
+    description?: string | null;
+  }): CategoryEntity {
+    // Aquí podrías añadir lógica de validación antes de crear el objeto
+    return new CategoryEntity(props);
   }
 }

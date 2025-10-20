86b49618be00f90ab037d15643c1e771d25ec0cc
Andres Quintero <andresfelipequintero2091@gmail.com>
2025-10-20 11:07:03 -0500

    feat: agregar caso de uso para obtener implemento por id de grupo de implemento- Añadidos caso de uso GetImplementByIdGroup.- Modificado IImplementRepository y SequelizeImplementRepository para cargar los implementos por id de grupo de implementos.- Ajustado GroupImplementService y estilos en index.css.- Añadidos nuevos componentes (Modal, Head, InputField) y validaciones de entrada.

--- backend/src/domain/interfaces/IImplementRepository.ts
+++ backend/src/domain/interfaces/IImplementRepository.ts
@@ -3,6 +3,7 @@ import {ImplementEntity} from "../entities/ImplementEntity";
 export interface IImplementRepository {
   findAll(): Promise<ImplementEntity[]>;
   findById(id: number): Promise<ImplementEntity | null>;
+  findByIdGroup(idGroup: number): Promise<ImplementEntity[]>;
   save(implement: ImplementEntity): Promise<ImplementEntity>;
   // update(implement: ImplementEntity): Promise<ImplementEntity>;
   // delete(id: number): Promise<void>;

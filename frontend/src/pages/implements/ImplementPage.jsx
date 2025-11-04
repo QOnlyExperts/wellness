b70f9afd814aecf9207b21055ec7a29b125f3625
Andres021 <andresfelipequintero2091@gmail.com>
2025-11-03 21:46:59 -0500

    Se añadieron componente visuales de pruebas para el home

--- frontend/src/pages/implements/ImplementPage.jsx
+++ frontend/src/pages/implements/ImplementPage.jsx
@@ -1,193 +1,82 @@
-import { useEffect, useState } from "react";
-import { useLoader } from "../../context/LoaderContext";
-import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { Link, useParams } from "react-router-dom";
+import { useImplements } from "../../hooks/useImplements";
 
-import ImplementService from "../../services/ImplementService";
 import AlertContainer from "../../containers/shared/AlertContainer";
 import Head from "../../components/shared/Head";
-import HorizontalScroll from "../../components/shared/HorizontalScroll";
-import Card from "../../components/shared/Card";
 import Badge from "../../components/shared/Badge";
-
+import Card from "../../components/shared/Card";
 import DashboardChartsImplements from "../../components/implement/DashboardChartsImplements";
 
-import CheckIcon from "../../components/icons/CheckIcon"
-import ConfigIcon from "../../components/icons/Config"
-import CancelIcon from "../../components/icons/CancelIcon"
-import WarningIcon from "../../components/icons/WarningIcon"
+import CheckIcon from "../../components/icons/CheckIcon";
+import ConfigIcon from "../../components/icons/Config";
+import CancelIcon from "../../components/icons/CancelIcon";
+import WarningIcon from "../../components/icons/WarningIcon";
+
+import { translateStatus } from "../../utils/formatStatus";
+import NotFoundImage from "../../assets/img/NoImg.svg";
 
 import "./ImplementPage.css";
 
-import NotFoundImage from "../../assets/img/NoImg.svg";
-import { useParams } from "react-router-dom";
-import { translateStatus } from "../../utils/formatStatus";
-
 const ImplementPage = () => {
-  const { showLoader, hideLoader } = useLoader();
-  const [implementList, setImplementList] = useState([]);
-  const [statusList, setStatusList] = useState([]);
-  const [implementCategory, setImplementCategory] = useState([]);
   const { status } = useParams();
 
-  useEffect(() => {
-    const fetch = async () => {
-      if (!status) return; // si no hay status, no hacer la llamada
-      showLoader();
-      const statusList = [
-        "all",
-        "available",
-        "borrowed",
-        "maintenance",
-        "retired",
-      ];
-      let response;
+  const { implementList, statusList, implementCategory, error } = useImplements(status);
 
-      if (status === "all") {
-        response = await ImplementService.getImplements();
-
-        if (response.success) {
-          const countStatus = statusList.reduce((acc, status) => {
-            const found = response.data.filter((imp) => imp.status === status);
-            if (found.length > 0) {
-              acc.push({ status: status, amount: found.length });
-            }
-            return acc;
-          }, []);
-          // Output expected: [{ status: available, amount: 2 }]
-
-          // Filtramos las categorias y agrupamos su cantidad
-          const implementsCategory = Object.values(
-            response.data.reduce((acc, implement) => {
-              const category = implement.groupImplement?.name || "Sin categoría";
-
-              if (!acc[category]) {
-                acc[category] = { category, amount: 0 };
-              }
-
-              acc[category].amount += 1;
-              return acc;
-            }, {})
-          );
-
-
-          setStatusList(countStatus);
-          setImplementCategory(implementsCategory);
-
-        }
-      } else {
-        // const response = await GroupImplementService.getGroupImplements();
-        response = await ImplementService.getImplementsByStatus(status);
-      }
-
-      if (!response.success) {
-        window.showAlert(
-          response?.error.message || "Error al obtener los implementos",
-          "Error"
-        );
-        return;
-      }
-
-      setImplementList(response.data);
-      // window.showAlert(response?.message || "Implementos obtenidos exitosamente", "success");
-      hideLoader();
-    };
-
-    fetch();
-  }, [status]);
-
-  const renderBadge = (status) => {
-      return (
-        <div 
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            gap: '5px',
-          }}
-        >
-
-          {
-            status.status === "available" &&
-              <CheckIcon color="var(--color-available)" />
-          }
-          {
-            status.status === "borrowed" && 
-              <WarningIcon color="var(--color-borrowed)" />
-          }
-          {
-            status.status === "maintenance" && 
-              <ConfigIcon color="var(--color-maintenance)" />
-          }
-          {
-            status.status === "retired" && 
-              <CancelIcon color="var(--color-retired)" />
-          }
-          {translateStatus(status.status)}
-          {/* <Badge key={i} label={status.status} value={status.status}/> */}
-        </div>
-      )
+  const renderBadge = (status, total) => {
+    return (
+      <div className="statistics-icon">
+        {status.status === "available" && (
+          <CheckIcon size={35} color="var(--color-available)" />
+        )}
+        {status.status === "borrowed" && (
+          <WarningIcon size={35} color="var(--color-borrowed)" />
+        )}
+        {status.status === "maintenance" && (
+          <ConfigIcon size={35} color="var(--color-maintenance)" />
+        )}
+        {status.status === "retired" && (
+          <CancelIcon size={35} color="var(--color-retired)" />
+        )}
+        {total}
+      </div>
+    );
   };
+
+  if (error) {
+    window.showAlert(error, "Error");
+  }
 
   return (
     <div className="div-principal">
-      <AlertContainer />
-      <Head title="Estados de implementos">
-        <div
-          style={{
-            display: "flex",
-            width: "100%",
-            justifyContent: "space-between",
-            gap: "10px",
-          }}
-        >
-          <div
-            style={{
-              flex: 1, // 👈 ocupa espacio proporcional dentro del contenedor
-              background: "#ffffffff",
-              borderRadius: "12px",
-              padding: "20px",
-              textAlign: "center",
-              boxShadow: "var(--box-shadow)",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <span style={{ fontWeight: "bold", color: "#111827" }}>
-              {renderBadge({status: "all"})}
+      {/* <AlertContainer /> */}
+      <Head 
+        title="Estados de implementos"
+        subTitle="Gestión y control de estados e implementos"  
+      >
+        <div className="content-statistics">
+          <div className="statistics">
+            <span style={{ fontSize: "35px", color: "#111827" }}>
+              {renderBadge({ status: "all" }, implementList.length)}
             </span>
-            <span style={{ fontSize: "24px", color: "#111827" }}>
-              {implementList.length}
+
+            <span style={{ fontWeight: "bold", color: "#000000ff" }}>
+              {translateStatus("all")}
             </span>
           </div>
-          {statusList.length > 0 &&
-            statusList.map((status, i) => (
-              <div
-                key={i}
-                style={{
-                  flex: 1, // 👈 ocupa espacio proporcional dentro del contenedor
-                  background: "#ffffffff",
-                  borderRadius: "12px",
-                  padding: "20px",
-                  textAlign: "center",
-                  boxShadow: "var(--box-shadow)",
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <span style={{ fontWeight: "bold", color: "#111827" }}>
-                  {renderBadge(status)}
-                </span>
-                <span style={{ fontSize: "24px", color: "#111827" }}>
-                  {status.amount}
-                </span>
-              </div>
-            ))}
+          {statusList.map((status, i) => (
+            <div className="statistics">
+              <span style={{ fontSize: "35px", color: "#000000ff" }}>
+                {renderBadge(status, status.amount)}
+              </span>
+              <span style={{ fontWeight: "bold", color: "#111827" }}>
+                {translateStatus(status.status)}
+              </span>
+            </div>
+          ))}
         </div>
       </Head>
 
+      {/* Filtros por estado */}
       <div
         style={{
           display: "flex",
@@ -199,13 +88,14 @@ const ImplementPage = () => {
         <Link to={`/admin/implement/status/all`}>
           <Badge label={"all"} value={"all"} />
         </Link>
-        {statusList.length > 0 &&
-          statusList.map((status, i) => (
-            <Link to={`/admin/implement/status/${status.status}`}>
-              <Badge key={i} label={status.status} value={status.status} />
-            </Link>
-          ))}
+        {statusList.map((status, i) => (
+          <Link key={i} to={`/admin/implement/status/${status.status}`}>
+            <Badge label={status.status} value={status.status} />
+          </Link>
+        ))}
       </div>
+
+      {/* Contenido principal */}
       <div
         style={{
           display: "flex",
@@ -215,6 +105,7 @@ const ImplementPage = () => {
           gap: "15px",
         }}
       >
+        {/* Lista de implementos */}
         <div
           style={{
             display: "flex",
@@ -234,7 +125,6 @@ const ImplementPage = () => {
               implementList.map((imp, i) => (
                 <Card
                   key={i}
-                  // onClick={() => handleImplement(imp.id)}
                   type={imp.status}
                   images={
                     imp.imgs && imp.imgs.length > 0
@@ -243,7 +133,6 @@ const ImplementPage = () => {
                         )
                       : [NotFoundImage]
                   }
-                  // description={formImplement.status}
                 >
                   <div
                     style={{
@@ -271,7 +160,6 @@ const ImplementPage = () => {
                       value={imp.status || "available"}
                       label={imp.status || "available"}
                     />
-
                     <span>{imp.cod}</span>
                   </div>
                 </Card>
@@ -282,6 +170,7 @@ const ImplementPage = () => {
           </div>
         </div>
 
+        {/* Gráficas */}
         <div>
           <DashboardChartsImplements
             implementCategory={implementCategory}

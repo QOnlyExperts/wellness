be1eeadf25e49f66b0a221d28358f769201065b2
Andres021 <andresfelipequintero2091@gmail.com>
2025-12-11 05:09:19 -0500

    solucionando error de importacion

--- frontend/src/components/shared/SideBar.jsx
+++ frontend/src/components/shared/SideBar.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'; 
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 
-import './Sidebar.css'
+import './SideBar.css'
 import Arrow from '../../assets/arrow.svg';
 import Home from '../../assets/home.svg';
 import Dashboard from '../../assets/dashboard.svg';
